8f0f544879ecd24a354acab491912c0d9c4be98a
paudler <sara.paudler@student.hnu.de>
2024-12-25 15:21:44 +0100

    Buttons für die Monatstage erstellt sowie Variable "day" hinzugefügt, Zwischenspeicher unsichtbar gemacht

--- src/main/java/org/example/Projekt/ThePrivateJetAgency.java
+++ src/main/java/org/example/Projekt/ThePrivateJetAgency.java
@@ -2,6 +2,8 @@ package org.example.Projekt;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ThePrivateJetAgency extends JFrame {
     private JPanel frame;
@@ -73,6 +75,7 @@ public class ThePrivateJetAgency extends JFrame {
     private JButton buttonClear;
     private JButton buttonNurAktuelleReisen;
     private JButton alleReisenAnzeigenButton;
+    int day;
 
     public ThePrivateJetAgency () {
         setTitle("The Private Jet Agency");
@@ -82,7 +85,262 @@ public class ThePrivateJetAgency extends JFrame {
         setVisible(true);
 
         textAreaReiseAusgabe.setPreferredSize(new Dimension(1200,800));
+        textFieldZwischenspeicher.setVisible(false);
+
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                day = 1;
+                textFieldZwischenspeicher.setText(String.valueOf(day));
+            }
+        });
+
+        button2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                day = 2;
+                textFieldZwischenspeicher.setText(String.valueOf(day));
+            }
+        });
+
+        button3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                day = 3;
+                textFieldZwischenspeicher.setText(String.valueOf(day));
+            }
+        });
+
+        button4.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                day = 4;
+                textFieldZwischenspeicher.setText(String.valueOf(day));
+            }
+        });
+
+        button5.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                day = 5;
+                textFieldZwischenspeicher.setText(String.valueOf(day));
+            }
+        });
+
+        button6.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                day = 6;
+                textFieldZwischenspeicher.setText(String.valueOf(day));
+            }
+        });
+
+        button7.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                day = 7;
+                textFieldZwischenspeicher.setText(String.valueOf(day));
+            }
+        });
+
+        button8.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                day = 8;
+                textFieldZwischenspeicher.setText(String.valueOf(day));
+            }
+        });
+
+        button9.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                day = 9;
+                textFieldZwischenspeicher.setText(String.valueOf(day));
+            }
+        });
+
+        button10.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                day = 10;
+                textFieldZwischenspeicher.setText(String.valueOf(day));
+            }
+        });
+
+        button11.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                day = 11;
+                textFieldZwischenspeicher.setText(String.valueOf(day));
+            }
+        });
+
+        button12.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                day = 12;
+                textFieldZwischenspeicher.setText(String.valueOf(day));
+            }
+        });
+
+        button13.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                day = 13;
+                textFieldZwischenspeicher.setText(String.valueOf(day));
+            }
+        });
+
+        button14.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                day = 14;
+                textFieldZwischenspeicher.setText(String.valueOf(day));
+            }
+        });
+
+        button15.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                day = 15;
+                textFieldZwischenspeicher.setText(String.valueOf(day));
+            }
+        });
+
+        button16.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                day = 16;
+                textFieldZwischenspeicher.setText(String.valueOf(day));
+            }
+        });
+
+        button17.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                day = 17;
+                textFieldZwischenspeicher.setText(String.valueOf(day));
+            }
+        });
+
+        button18.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                day = 18;
+                textFieldZwischenspeicher.setText(String.valueOf(day));
+            }
+        });
+
+        button19.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                day = 19;
+                textFieldZwischenspeicher.setText(String.valueOf(day));
+            }
+        });
+
+        button20.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                day = 20;
+                textFieldZwischenspeicher.setText(String.valueOf(day));
+            }
+        });
+
+        button21.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                day = 21;
+                textFieldZwischenspeicher.setText(String.valueOf(day));
+            }
+        });
+
+        button22.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                day = 22;
+                textFieldZwischenspeicher.setText(String.valueOf(day));
+            }
+        });
+
+        button23.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                day = 23;
+                textFieldZwischenspeicher.setText(String.valueOf(day));
+            }
+        });
+
+        button24.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                day = 24;
+                textFieldZwischenspeicher.setText(String.valueOf(day));
+            }
+        });
+
+        button25.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                day = 25;
+                textFieldZwischenspeicher.setText(String.valueOf(day));
+            }
+        });
+
+        button26.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                day = 26;
+                textFieldZwischenspeicher.setText(String.valueOf(day));
+            }
+        });
+
+        button27.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                day = 27;
+                textFieldZwischenspeicher.setText(String.valueOf(day));
+            }
+        });
+
+        button28.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                day = 28;
+                textFieldZwischenspeicher.setText(String.valueOf(day));
+            }
+        });
+
+        button29.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                day = 29;
+                textFieldZwischenspeicher.setText(String.valueOf(day));
+            }
+        });
+
+        button30.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                day = 30;
+                textFieldZwischenspeicher.setText(String.valueOf(day));
+            }
+        });
+
+        button31.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                day = 31;
+                textFieldZwischenspeicher.setText(String.valueOf(day));
+            }
+        });
+
+
+
+
     }
+
+
 
     public static void main(String[] args) {
         ThePrivateJetAgency tpja1 = new ThePrivateJetAgency();
