dae68d93f34d7c1060022d1c3141d6e734ca54bc
7onah <jonah.suiter@student.hnu.de>
2025-01-11 13:02:34 +0100

    Erstellen eines JUnit Tests, der die Methode "reisedaten" testet. Ein Objekt "testreise" wird definiert und mit Eingabeparametern belegt. Die erwartete Ausgabe und die tatsächliche Ausgabe (Rückgabe der Methode "reisedaten" bei Eingabe des Objekts "testreise") wird auf Übereinstimmung getestet.

---
+++ src/test/java/org/example/Projekt/reisedatenTest.java
@@ -0,0 +1,31 @@
+package org.example.Projekt;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class reisedatenTest {
+
+    // Ein Objekt "testreise" wird definiert und mit Eingabeparametern belegt
+    Reise testreise = new Reise("Reise steht noch aus", "München", "Las Palmas de Gran Canaria", "16:00", 2025, "August", 6, 3, "Trüffel Pasta", "Hotel Bergblick", "klein");
+
+    @Test
+    // Es wird die erwartete Ausgabe für die Eingabeparameter definiert.
+    // Die erwartete Ausgabe und die tatsächliche Ausgabe (Rückgabe der Methode "reisedaten" bei Eingabe des Objekts "testreise") wird auf Übereinstimmung getestet.
+    void reiselisteTest(){
+        assertEquals("Reisestatus: " + "Reise steht noch aus"
+                +"\n"
+                + "Flug von " + "München" + " nach " + "Las Palmas de Gran Canaria" + "\n"
+                +"\n"
+                + "Abflug um " + "16:00" + " den " + 6 + ". " + "August" + " " + 2025 + "\n"
+                + "Anzahl der Reisenden: " + 3 + "\n"
+                + "Gewählte Flugzeuggröße: " + "klein" + "\n"
+                + "Kulinarische Versorgung: " + "Trüffel Pasta" +"\n"
+                + "Abholservice: " + "Hotel Bergblick" +"\n"
+                +"\n"
+                +"\n"
+                + "----------------------------------------------------------"
+                +"\n",
+                testreise.reisedaten());
+    }
+}
